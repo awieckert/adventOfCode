8bbdc5bb4d734992ee9367ffbb7526925ded0dea
awieckertNCS <adam.wieckert@ncontracts.com>
2018-12-04 09:17:18 -0600

    WI{

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace adventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ day2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace adventOfCode
+{
+    public class day2
+    {
+        private IEnumerable<string> ListOIds { get; set; }
+
+        day2()
+        {
+            ListOIds = File.ReadAllLines("C:\\Users\\AdamWieckert\\Documents\\QA\\Reday2.txt");
+        }
+    }
+}
